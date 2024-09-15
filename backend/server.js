104497366ff9da137b5d5333a5ba1daa04d97af8
Darshit <darshitkakadiya1@gmail.com>
2024-09-15 16:53:26 +0530

    Revert "remove"
    This reverts commit 1b72a24b644575e9ef3834933babea2d89b08c35.

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
