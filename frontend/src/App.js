b5f65d9c53f20059acedef27104d37e27669c95f
Darshit <darshitkakadiya1@gmail.com>
2024-09-15 19:32:13 +0530

    add shopify set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import en from '@shopify/polaris/locales/en.json';//shopify polaris
+import { AppProvider } from '@shopify/polaris';//shopify polaris
+import '@shopify/polaris/build/esm/styles.css';//shopify polaris
+import Test from './components/Testing/Test';
 
 function App() {
   return (
-    <>
-  
-    </>
+    <AppProvider i18n={en}>
+      <Test />
+    </AppProvider>
   );
 }
 
---
+++ frontend/src/components/Testing/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@shopify/polaris';
+
+const Test = () => {
+    return (
+        <div>
+            <Button>Add product</Button>
+        </div>
+    )
+}
+
+export default Test
