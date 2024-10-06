0887f8fe6b71fad1bab0d431a48ca15494eda3c2
Darshit <darshitkakadiya1@gmail.com>
2024-10-06 23:27:06 +0530

    set design

--- frontend/src/components/Login/LoginCard.js
+++ frontend/src/components/Login/LoginCard.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Page, Card } from '@shopify/polaris';
+import { Page, Card, Box } from '@shopify/polaris';
 import AuthCard from './AuthCard';
 
 function LoginCard(props) {
   return (
     <Page>
-      <Card >
-        <AuthCard props={props} />
-      </Card>
+      {/* <Card > */}
+      <Box padding={2400}>
+        <Card>
+          <AuthCard props={props} />
+        </Card>
+      </Box>
+      {/* </Card> */}
     </Page>
   )
 }
