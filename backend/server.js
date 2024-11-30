7235dbbda690512b25ea717cfed2f4fd196efba8
Darshit_Wiser <darshit.wiser@gmail.com>
2024-11-30 18:30:28 +0530

    collaborat

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('./config/global');
 require('./collection/index');//modal
 require('./config/route')//route
 
-// Start the server
+// Start the server...
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}...`);
