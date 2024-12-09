aff44458a8770bab6cd66198aab098c7ad1d868c
Darshit_Wiser <darshit.wiser@gmail.com>
2024-12-09 17:19:10 +0530

    single fiile notes design

--- frontend/src/containers/Dashboard/Dashboard.js
+++ frontend/src/containers/Dashboard/Dashboard.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Page, Card, Grid } from '@shopify/polaris';
 
 const Dashboard = () => {
   return (
     <div>
-      dashboard
+      <Page
+        title="Notes"
+        subtitle="Your Digital Notebook"
+      >
+        <Card>
+          <Grid>
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <Card title="Sales" sectioned>
+                <p>View a summary of your online store’s sales.</p>
+              </Card>
+            </Grid.Cell>
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <Card title="Orders" sectioned>
+                <p>View a summary of your online store’s orders.</p>
+              </Card>
+            </Grid.Cell>
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <Card title="Orders" sectioned>
+                <p>View a summary of your online store’s orders.</p>
+              </Card>
+            </Grid.Cell>
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <Card title="Orders" sectioned>
+                <p>View a summary of your online store’s orders.</p>
+              </Card>
+            </Grid.Cell>
+          </Grid>
+        </Card>
+      </Page>
     </div>
   )
 }
