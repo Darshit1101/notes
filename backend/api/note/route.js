3c935318ba20a3d673d3f9e94ee829181e4918db
Darshit_Wiser <darshit.wiser@gmail.com>
2025-02-17 17:04:13 +0530

    middleware

--- backend/api/note/route.js
+++ backend/api/note/route.js
@@ -1,7 +1,8 @@
 const Ctrl = require('./controller');
+const { checkAuth } = require('../../config/middleware');
 
 var app = module.exports = express.Router();
-app.route(env.API_PREFIX + '/addnote').post(Ctrl.addNote);
+app.route(env.API_PREFIX + '/addnote').post(checkAuth, Ctrl.addNote);
 app.route(env.API_PREFIX + '/getAllNotes').post(Ctrl.getAllNotes);
 app.route(env.API_PREFIX + '/deleteCard').delete(Ctrl.deleteCard);
 app.route(env.API_PREFIX + '/editNote').post(Ctrl.editNote);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('./config/global');
 require('./collection/index');//modal
 require('./config/route')//route
+require('./config/middleware');//middleware (checkAuth)
 
 // Start the server...
 const PORT = process.env.PORT;
