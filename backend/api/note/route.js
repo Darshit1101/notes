5f117aac601a28e172cdac04faac72d891229875
Darshit_Wiser <darshit.wiser@gmail.com>
2025-02-24 11:42:32 +0530

    checkAuth add in all api

--- backend/api/note/route.js
+++ backend/api/note/route.js
@@ -3,6 +3,6 @@ const { checkAuth } = require('../../config/middleware');
 
 var app = module.exports = express.Router();
 app.route(env.API_PREFIX + '/addnote').post(checkAuth, Ctrl.addNote);
-app.route(env.API_PREFIX + '/getAllNotes').post(Ctrl.getAllNotes);
-app.route(env.API_PREFIX + '/deleteCard').delete(Ctrl.deleteCard);
-app.route(env.API_PREFIX + '/editNote').post(Ctrl.editNote);
+app.route(env.API_PREFIX + '/getAllNotes').post(checkAuth, Ctrl.getAllNotes);
+app.route(env.API_PREFIX + '/deleteCard').delete(checkAuth, Ctrl.deleteCard);
+app.route(env.API_PREFIX + '/editNote').post(checkAuth, Ctrl.editNote);
--- backend/api/profile/route.js
+++ backend/api/profile/route.js
@@ -1,4 +1,5 @@
 const Ctrl = require('./controller');
+const { checkAuth } = require('../../config/middleware');
 
 var app = module.exports = express.Router();
-app.route(env.API_PREFIX + '/getProfile').get(Ctrl.getProfile);
+app.route(env.API_PREFIX + '/getProfile').get(checkAuth, Ctrl.getProfile);
